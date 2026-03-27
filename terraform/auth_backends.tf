93f319683ddbc8bdbe9cbae53f9d3538cbbfbba2
Jay Kroger <jaykroger@labrynth.cloud>
2026-03-27 19:27:06 -0400

    adding comments

--- terraform/auth_backends.tf
+++ terraform/auth_backends.tf
@@ -4,12 +4,15 @@
 
 
 ### Kubernetes Auth Backend ###
+
+# Backend
 resource "vault_auth_backend" "kubernetes" {
     type        = "kubernetes"
     path        = "kubernetes"
     description = "Kubernetes auth backend for pod identity authentication."
 }
 
+# Backend Config
 resource "vault_kubernetes_auth_backend_config" "kubernetes" {
     backend                = vault_auth_backend.kubernetes.path
     kubernetes_host        = var.kubernetes_host
@@ -17,6 +20,7 @@ resource "vault_kubernetes_auth_backend_config" "kubernetes" {
     disable_iss_validation = true 
 }
 
+# Backend Roles
 resource "vault_kubernetes_auth_backend_role" "cert_manager" {
     backend                          = vault_auth_backend.kubernetes.path
     role_name                        = "cert-manager"
@@ -30,14 +34,19 @@ resource "vault_kubernetes_auth_backend_role" "cert_manager" {
 
 ### GitHub JWT Auth Backend ###
 
+# Backend
 resource "vault_auth_backend" "github" {
     type        = "jwt"
     path        = "jwt"
     description = "JWT auth backend for GitHub Actions workflows."
 }
 
+# Backend Config
 resource "vault_jwt_auth_backend_config" "github" {
     backend           = vault_auth_backend.github.path
     oidc_discover_url = "https://token.actions.githubusercontent.com"
     bound_issuer      = "https://token.actions.githubusercontent.com"
 }
+
+# Backend Roles
+
